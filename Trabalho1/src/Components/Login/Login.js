3b3d7a8bc7edf6d1a4e677c487a38c41c52477c3
unknown <filippemkt@gmail.com>
2020-10-01 23:33:01 -0300

    Insert icon to login screen

--- Trabalho1/src/Components/Login/Login.js
+++ Trabalho1/src/Components/Login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Image, Text, TextInput, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Login = () => {
 
@@ -13,10 +14,12 @@ const Login = () => {
                 <TextInput
                     style={{ marginTop: 30, height: 35, borderColor: 'gray', borderWidth: 1 }}
                     onChangeText={text => onChangeText(text)}
+                    placeholder="Login"
                 />
                 <TextInput
                     style={{ marginTop: 20, height: 35, borderColor: 'gray', borderWidth: 1 }}
                     onChangeText={text => onChangeText(text)}
+                    placeholder="Senha"
                 />
                 <View style={{ padding: 15 }}>
                     <Button
@@ -27,6 +30,10 @@ const Login = () => {
                 </View>
                 <View style={{ marginTop: 20, height: 250, width: 370, marginLeft: 130 }} >
                     <Text>Esqueceu a senha?</Text>
+                </View>
+                <View style={{marginTop: -200, marginLeft:150, flexDirection:"row"}}>
+                    <Text style={{textAlignVertical:"center"}}>Entre com </Text>
+                <Icon name="facebook-square" size={30} color="#3b5998" />
                 </View>
             </View>
         </View>
